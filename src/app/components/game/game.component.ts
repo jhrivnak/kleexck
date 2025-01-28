320ef7ee94a53c1ec9db85e21a787772394e729b
jhrivnak <118409231+jhrivnak@users.noreply.github.com>
2025-01-27 22:06:04 -0500

    Change player from green circle to red square

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -20,7 +20,7 @@ const INITIAL_PLAYER: Player = {
   template: `
     <div class="game-container">
       <div class="game-info">
-        <p>Use arrow keys or WASD to move the green circle</p>
+        <p>Use arrow keys or WASD to move the red square</p>
       </div>
       <div class="health-container">
         <div class="hearts">
@@ -174,17 +174,14 @@ export class GameComponent implements OnInit, AfterViewInit, OnDestroy {
       // Clear canvas
       this.ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-      // Draw player as a green circle
-      this.ctx.fillStyle = 'green';
-      this.ctx.beginPath();
-      this.ctx.arc(
-        this.gameState.player.x + this.gameState.player.width / 2, 
-        this.gameState.player.y + this.gameState.player.height / 2, 
-        this.gameState.player.width / 2, 
-        0, 
-        Math.PI * 2
+      // Draw player as a red square
+      this.ctx.fillStyle = 'red';
+      this.ctx.fillRect(
+        this.gameState.player.x, 
+        this.gameState.player.y, 
+        this.gameState.player.width, 
+        this.gameState.player.height
       );
-      this.ctx.fill();
 
       // Draw other objects as blue squares
       this.gameState.objects.forEach(obj => {
